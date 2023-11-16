378aeacc996d35c7e7b69d77a3e701c0b83e8103
Moprogrammer1384 <69991167+Moprogrammer1384@users.noreply.github.com>
2023-11-16 16:34:55 +0330

    Adding Lesson class to project

---
+++ Lesson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Lesson
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Mark  { get; set; }
+        public byte Unit { get; set; }
+    }
+}
